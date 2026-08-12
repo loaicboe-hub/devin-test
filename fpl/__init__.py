deb11220d02308d14d938f33896a60eb68fdbd66
loaicboe-hub <loai.cboe@gmail.com>
2026-08-12 21:07:51 +0300

    Delete fpl directory

--- fpl/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Fetch and analyse data from the official Fantasy Premier League API."""
-
-from .client import FPLClient, FPLError
-from .models import Bootstrap, Gameweek, Player, Team
-
-__all__ = ["FPLClient", "FPLError", "Bootstrap", "Gameweek", "Player", "Team"]
